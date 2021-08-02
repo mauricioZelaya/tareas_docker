696f53a40c993841a707ea0f76386f39232d4c24
Mauricio <zelaya.mauricio@gmail.com>
2021-08-02 04:24:50 +0000

    last script update

--- homework2.py
+++ homework2.py
@@ -1,17 +1,21 @@
 import socket
 from datetime import date, datetime, timezone
 
-today = date.today()
-now = datetime.now()
-time_zone = datetime.now(timezone.utc).astimezone().tzname()
-hostname = socket.gethostname()
-local_ip = socket. gethostbyname(hostname)
-# dd/mm/YY
-d1 = today.strftime("%d/%m/%Y")
-current_time = now.strftime("%H:%M:%S")
-print("Current date:", d1)
-print("Current Time =", current_time)
-print("Time Zone: ",time_zone)
-print("hostname: ", hostname)
-print("IP address: ", local_ip)
+from flask import  Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def homework2():
+    today = date.today()
+    now = datetime.now()
+    time_zone = datetime.now(timezone.utc).astimezone().tzname()
+    hostname = socket.gethostname()
+    local_ip = socket. gethostbyname(hostname)
+    # dd/mm/YY
+    d1 = today.strftime("%d/%m/%Y")
+    current_time = now.strftime("%H:%M:%S")
+    return "Current date: " + d1 + " - Current Time =" + current_time + " - Time Zone: " + time_zone + " - hostname: " + hostname + " - IP address: " + local_ip
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
